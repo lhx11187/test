f0a9cf8a897503ce2ba7721de22bdf7d02ecf3e2
Blackduck <lhx11187@qq.com>
2017-05-22 17:30:50 +0800

    Update Youku.sh

--- SH/Youku.sh
+++ SH/Youku.sh
@@ -3,14 +3,26 @@
 #启动n2n脚本
 killall edge
 killall edge2
+
+
 /etc/storage/bin/n2n/edge -d n2n_v1 -a 10.10.1.13 -c blackduck -k 123 -l kai.lucktu.com:10082 &
-#/etc/storage/bin/n2n/edge2 -d n2n_v2 -a 10.10.10.13 -c blackduck -k 123 -l kai.lucktu.com:10086 &
-
-/etc/storage/bin/n2n/edge -d lucktu -a 10.10.4.13 -c blackduck -k 123 -l n2n.lucktu.com:10082 &
-
+#/etc/storage/bin/n2n/edge -d lu8_v1 -a 10.10.2.13 -c blackduck -k 123 -l n2n.lu8.win:10082 &
 #/etc/storage/bin/n2n/edge -d txdn_v1 -a 10.10.3.13 -c blackduck -k 123 -l n2n.txdn.tk:10082 &
+#/etc/storage/bin/n2n/edge -d lucktu -a 10.10.4.13 -c blackduck -k 123 -l n2n.lucktu.com:10082 &
+/etc/storage/bin/n2n/edge -d udpfile_v1 -a 10.10.5.13 -c blackduck -k 123 -l n2n.udpfile.com:10082 &
+/etc/storage/bin/n2n/edge -d llfj_v1 -a 10.10.6.13 -c blackduck -k 123 -l n2n.llfj.party:10082 &
+/etc/storage/bin/n2n/edge -d laiyx_v1 -a 10.10.7.13 -c blackduck -k 123 -l n2ndx.laiyx.win:10082 &
+/etc/storage/bin/n2n/edge -d rb_v1 -a 10.10.8.13 -c blackduck -k 123 -l 106.186.30.16:6489 &
+#/etc/storage/bin/n2n/edge -d laiyx_mg_v1 -a 10.10.9.13 -c blackduck -k 123 -l n2n.laiyx.win:10082 &
 
-/etc/storage/bin/n2n/edge -d lu8_v1 -a 10.10.2.13 -c blackduck -k 123 -l n2n.lu8.win:10082 &
+#捷克	remoteqth.com:82
+
+
+
+
+
+
+#/etc/storage/bin/n2n/edge2 -d n2n_v2 -a 10.10.10.13 -c blackduck -k 123 -l kai.lucktu.com:10086 &
 #/etc/storage/bin/n2n/edge2 -d lu8_v2 -a 10.10.20.13 -c blackduck -k 123 -l n2n.lu8.win:10086 &
 
 logger -t "【N2N启动脚本】" "脚本完成"
