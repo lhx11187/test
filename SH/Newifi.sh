8ae6a8e42c5547f97e4152dea854391adf3996fe
Blackduck <lhx11187@qq.com>
2017-07-17 08:50:51 +0800

    Update Newifi.sh

--- SH/Newifi.sh
+++ SH/Newifi.sh
@@ -10,7 +10,7 @@ killall edge2
 #/etc/storage/bin/n2n/edge -d lucktu -a 10.10.4.11 -c blackduck -k 123 -l n2n.lucktu.com:10082 &
 #/etc/storage/bin/n2n/edge -d udpfile_v1 -a 10.10.5.11 -c blackduck -k 123 -l n2n.udpfile.com:10082 &
 /etc/storage/bin/n2n/edge -d llfj_v1 -a 10.10.6.11 -c blackduck -k 123 -l n2n.llfj.party:10082 &
-#/etc/storage/bin/n2n/edge -d laiyx_v1 -a 10.10.7.11 -c blackduck -k 123 -l n2ndx.laiyx.win:10082 &
+/etc/storage/bin/n2n/edge -d laiyx_v1 -a 10.10.7.11 -c blackduck -k 123 -l n2n.laiyx.win:10082 &
 /etc/storage/bin/n2n/edge -d rb_v1 -a 10.10.8.11 -c blackduck -k 123 -l 106.186.30.16:6489 &
 #/etc/storage/bin/n2n/edge -d laiyx_mg_v1 -a 10.10.9.11 -c blackduck -k 123 -l n2n.laiyx.win:10082 &
 
